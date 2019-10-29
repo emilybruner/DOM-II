11e7ee83488700b2acc6d1b3d8aefb889f9ba090
Emily <bruner.emilym@gmail.com>
2019-10-29 16:31:50 -0400

    added

--- js/index.js
+++ js/index.js
@@ -7,16 +7,28 @@ document.querySelectorAll('.img-content').forEach(el => {
     })
     el.addEventListener("mouseleave", () => {
         el.style.transform = "scale(1)";
-        el.style.transition = "all 0.3s"
+        el.style.transition = "all 0.3s";
     })
 })
 
 let bus = document.querySelector(".funbusimg");
 
-bus.addEventListener("mouseover", (event) => {
+bus.addEventListener("mouseover", () => {
     bus.src = "/img/beach1.jpg"
 })
 
-bus.addEventListener("dblclick", (event) => {
+bus.addEventListener("dblclick", () => {
     bus.src = "/img/fun-bus.jpg"
+})
+
+document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener("click", () => {
+        el.style.color = 'hotpink'
+    })
+})
+
+const mainStuff = document.querySelector('.home');
+window.addEventListener("resize", () => {
+    mainStuff.style.backgroundColor = "navy";
+    mainStuff.style.color = "white";
 })
